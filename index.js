b5a349e32b95b57c0a7874f492e1cb0eaff49879
Danny Lee <dreamer@panix.com>
2020-01-24 12:14:47 -0500

    Add functions

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
-function findMinAndRemove(array){
+function findMinAndRemove (array) {
+  let minimum = array[0]
+  let minIdx = 0
 
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] < minimum) {
+      minimum = array[i]
+      minIdx = i
+    }
+  }
+  array.splice(minIdx, 1)
+  return minimum
 }
 
-function insertionSort(array){
-
+function insertionSort (array) {
+  const sorted = []
+  let min
+  while (array.length !== 0) {
+    min = findMinAndRemove(array) 
+    sorted.push(min)
+  }
+  return sorted
 }
